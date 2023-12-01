cf5f1bdfcec07faec73bc7ee8ecd22ba720b4b4b
luisffee <luizffeelix@gmail.com>
2023-12-01 14:38:26 -0300

    :sparckles: feat(API): Working JSON Results displayed on screen

--- app/static/scripts.js
+++ app/static/scripts.js
@@ -4,18 +4,32 @@ $(document).ready(function () {
         event.preventDefault();
 
         var $searchField = $('#search');
-        var $submitButton = $('#submit');
 
-        $searchField.prop("disabled", true);
-        $submitButton.attr("disabled", true).val("searching ...");
 
-        var flickerAPI = "https://api.semanticscholar.org/graph/v1/paper/search?";
-        var animal = $searchField.val();
-        var flickrOptions = {
-            query: "Atom",
-            format: "json",
+        var API = "https://openlibrary.org/search.json?";
+        var input = $searchField.val();
+        console.log(input);
+        var Options = {
+            q: input,
             limit: 10
+
         };
-        $.getJSON(flickerAPI, flickrOptions);
+        function displayResults(data) {
+            var resultHTML = '<ul>'
+            console.log(data.docs)
+            $.each(data.docs, function (i, result) {
+                resultHTML += '<li class="result">';
+                resultHTML += '<img class="result-img" src="https://covers.openlibrary.org/b/olid/' + result.cover_edition_key + '.jpg">';
+                resultHTML += '<h2 class="title">' + result.title + '</h2>';
+                resultHTML += '<p class="authors">' + result.author_name[0] + '</p>';
+                resultHTML += '<a class="link" + href="https://openlibrary.org' + result.key + '" >Link</a>';
+                resultHTML += '</li>';
+            });
+            resultHTML += '</ul>';
+            $('#search-results').html(resultHTML);
+
+        };
+        $.getJSON(API, Options, displayResults);
     })
+
 })
